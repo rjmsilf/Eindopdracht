eb61753f1c979ae19e7b5421a56d4355b5e402b1
rjmsilf <rjmsilfhout@gmail.com>
2015-06-27 10:09:50 +0000

    test of branch is gelukt

---
+++ testRobert2.py
@@ -0,0 +1,18 @@
+from Symbolische_manipulatie import *
+
+o=Constant(0)
+a=Constant(1)
+b=Constant(2)
+c=Constant(3)
+d=Constant(4)
+e=CosNode(2)
+x=Variable('x')
+y=Variable('y')
+z=Variable('z')
+
+iets=(x**b)**c
+#print(iets.simplify())
+print(iets.derivative(x))
+print(iets.hasvariable(x))
+
+
